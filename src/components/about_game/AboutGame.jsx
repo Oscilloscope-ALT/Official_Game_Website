3ca9ce69b8770e05263efb46946455b495fab0df
Oscilloscope-ALT <sofavaulina00365@gmail.com>
2026-05-26 17:21:07 +0300

    Добавлен контент в раздел "где играть"

--- src/components/about_game/AboutGame.jsx
+++ src/components/about_game/AboutGame.jsx
@@ -1,9 +1,22 @@
 import "./AboutGame.css"
+import ShtrihMini from "../../assets/imgs/Shtrih_dialogue.png"
+import Boots from "../../assets/imgs/Boots.png"
 
 export default function AboutGame(){
     return(
         <section className='about-game' id='about-game'>
           <h2>About game</h2>
+          <div className="about-content">
+            <div className="about-img">
+                <img src={ShtrihMini} alt="" />
+            </div>
+            <div className="about-txt">Shtrik the cat and friends is single-player browser game. 
+                What do you expect when you see this cat? Just play with this cute Shtrik and you'll find out where his friends are...
+                or... nevermind, just play with him. But please, be patient before you start or you will understand...</div>
+          </div>
+          <div className="about-end">
+            <img src={Boots} alt="" />
+          </div>
         </section>
     );
 }
--- src/components/where_to_play/WhereToPlay.jsx
+++ src/components/where_to_play/WhereToPlay.jsx
@@ -1,9 +1,22 @@
 import "./WhereToPlay.css"
+import GitHubLogo from "../../assets/imgs/GitHub.png"
+import SteamLogo from "../../assets/imgs/Steam.png"
 
 export default function WhereToPlay(){
     return(
         <section id='where-to-play'>
             <h2>Where to play</h2>
+            <div className="to-play-content">
+                <figure className="github-logo">
+                    <img src={GitHubLogo} alt="" />
+                    <figcaption>already available</figcaption>
+
+                </figure>
+                <figure className="steam-logo">
+                    <img src={SteamLogo} alt="" />
+                    <figcaption>soon</figcaption>
+                </figure>
+            </div>
         </section>
     );
 }
