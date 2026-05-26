6bc60cf2d2656293895f60a154df5842459864de
Oscilloscope-ALT <sofavaulina00365@gmail.com>
2026-05-26 18:12:32 +0300

    Сделан макет и загружены фото для раздела с ost

--- src/components/soundtracks/Soundtracks.jsx
+++ src/components/soundtracks/Soundtracks.jsx
@@ -1,9 +1,27 @@
 import "./Soundtracks.css"
+import Cat from "../../assets/imgs/Cat.png"
+import Dogs from "../../assets/imgs/Dogs.png"
 
 export default function Soundtracks(){
     return(
         <section id='soundtracks'>
           <h2>soundtracks</h2>
+          <div className="ost-intro">
+            We love music!
+          </div>
+          <div className="ost-content">
+            <div className="cat-img">
+                <img src={Cat} alt="" />
+            </div>
+            <div className="ost-list">
+                <ul>
+                    <li>mike shinoda</li>
+                </ul>
+            </div>
+            <div className="dog-img">
+                <img src={Dogs} alt="" />
+            </div>
+          </div>
         </section>
     );
 }
