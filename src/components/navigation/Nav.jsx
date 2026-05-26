ebe8d33adc4080f4fdd4db3381ae14cdab73bc7e
Oscilloscope-ALT <sofavaulina00365@gmail.com>
2026-05-26 14:20:20 +0300

    Обновлен и адаптирован размер шрифта в меню навигации

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -6,9 +6,9 @@ export default function Nav(){
         <ul className='nav-list'>
           <li><a href="#about-game">about game</a></li>
           <li><a href="#soundtracks">soundtracks</a></li>
-          <li><a href="#where-to-play">where-to-play</a></li>
-          <li><a href="#soundtracks">special-thanks</a></li>
-          <li><a href="#about-us">about-us</a></li>
+          <li><a href="#where-to-play">where to play</a></li>
+          <li><a href="#soundtracks">special thanks</a></li>
+          <li><a href="#about-us">about us</a></li>
           <li><a href="#about-us">contacts</a></li>         
         </ul>
       </nav>
